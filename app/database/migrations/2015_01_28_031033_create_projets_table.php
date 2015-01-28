5848205bbd493d948f1418ef2226ca1619d77704
Cary Cheng <c4cheng@ucsd.edu>
2015-01-27 20:31:54 -0800

    migrations added

---
+++ app/database/migrations/2015_01_28_031033_create_projets_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjetsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('projects', function(Blueprint $table)
+		{
+			// $table->increments('id');
+			// $table->timestamps();
+			// $table->
+				$table->increments('id');
+				$table->integer('projects_id');
+				$table->string('rating');
+				$table->mediumText('description');
+				$table->timestamp('DateTimeAdded');
+				$table->string('Category', 50);
+				$table->integer('hoursNeeded');
+				$table->integer('teamSize');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('projects');
+	}
+
+}
---
+++ app/database/migrations/2015_01_28_032113_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+				$table->increments('id');
+				$table->integer('users_id');
+				$table->string('name');
+				$table->string('projects_worked_on');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
