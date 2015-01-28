3fc46ada304e4bee711c13ba693f89a288acf7f6
Jason Ruan <emailme1580@yahoo.com>
2015-01-27 22:11:42 -0800

    projects controller
    Work in progress of api methods

---
+++ app/controllers/ProjectsController.php
@@ -0,0 +1,42 @@
+<?php
+	class ProjectsController extends BaseController{
+		public function postProject(){
+			if(Input:has('description', 'Category', 'hoursNeeded', 'teamSize')){
+					$project = new Project;
+					$project->description = $input['description'];
+					$project->Category = $input['Category'];
+					$project->hoursNeeded = $input['hoursNeeded'];
+					$project->teamSize = $input['teamSize'];
+					$customer->save();
+					return $customer;
+			}
+			else
+				return Response:make("Fill in all the fields");
+		}
+		
+		public function findProject(){
+			if(Input:has('Category')){
+				$category = Input::get('Category');
+				$result = DB::table('projects')->where('Category', '=', $category)->get();
+				return $result;
+			}
+			else{
+				return Response:make("Fill in all the fields");
+			}
+		}
+		
+		public function deletePost(){
+			$current_time = date('Y-m-d H:i:s', time());
+			$now = strtotime($current_time);
+			
+		}
+		
+		public function updateRate(){
+			if(Input:has('projects_id')){
+				$id = Input::get('project_id');
+				$project = DB::table('projects')->where('project_id', '=', $id)->get();
+				DB::table('projects')->where('project_id', '=', $id)->update(array('rating'=>$project->rating + 1));
+			}
+		}
+	}
+?>
