9d69e76e809cd25f4caaec3a00966dfa1ba423f9
Cary Cheng <c4cheng@ucsd.edu>
2015-01-28 18:09:52 -0800

    seeding works

--- app/models/Project.php
+++ app/models/Project.php
@@ -1,7 +1,8 @@
 <?php 
 	class Project extends Eloquent{
 
-		protected $fillable = array('description', 'hoursNeeded', 'teamSize');
+		public $timestamps = false; 
+		protected $fillable = array('description', 'hoursNeeded');
 
 		public function  users(){
 			return $this->belongsToMany('Users', 'projects_users', 'projects_id', 'users_id');
--- app/models/User.php
+++ app/models/User.php
@@ -1,7 +1,8 @@
 <?php
 	class User extends Eloquent{
 		//need to include updated_at and created_at parameters to utilize timestamp()
-		protected $fillable = array('name', 'projects_worked_on', 'updated_at', 'created_at');
+		public $timestamps = false; 
+		protected $fillable = array('name', 'projects_worked_on');
 
 		public function projects(){
 			return $this->belongsToMany('Project' , 'projects_users', 'users_id', 'projects_id');
